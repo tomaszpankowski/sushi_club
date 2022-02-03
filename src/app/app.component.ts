90dc0bcbd426bdf1b79f24b7d61178c098457fa0
tomaszpankowski <--get-all>
2022-02-03 02:38:15 +0100

    routing modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [
-    './app.component.sass',
     './sass/styles.scss'
     ]
 })
